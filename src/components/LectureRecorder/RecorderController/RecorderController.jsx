067855e580f7de8a4b7d2df3f668c909f505d33b
raj <malviyaraj906@gmail.com>
2024-10-22 21:19:35 +0530

    access key

--- src/components/LectureRecorder/RecorderController/RecorderController.jsx
+++ src/components/LectureRecorder/RecorderController/RecorderController.jsx
@@ -98,3 +98,13 @@ const RecorderController = ({
 };
 
 export default RecorderController;
+
+export const formatTime = (timeInMS) => {
+  const timeInSeconds = Math.floor(timeInMS / 1000);
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+    2,
+    "0"
+  )}`;
+};
--- src/hooks/AwsSdk.jsx
+++ src/hooks/AwsSdk.jsx
@@ -1,14 +1,15 @@
-import AWS from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
 
 export const AwsSdk = () => {
   const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
   const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-  AWS.config.update({
-    accessKeyId: awsAccessKeyId,
-    secretAccessKey: awsSecretAccessKey,
+  const s3 = new S3Client({
+    credentials: {
+      accessKeyId: awsAccessKeyId,
+      secretAccessKey: awsSecretAccessKey,
+    },
   });
 
-  const s3 = new AWS.S3();
   return { s3 };
 };
