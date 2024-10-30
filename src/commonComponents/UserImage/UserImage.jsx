25be13601c88ffcd9146eb58c3dfada9af957aad
raj <malviyaraj906@gmail.com>
2024-10-30 14:33:00 +0530

    bug fix

--- src/commonComponents/UserImage/UserImage.jsx
+++ src/commonComponents/UserImage/UserImage.jsx
@@ -8,16 +8,12 @@ import Cookies from "js-cookie";
 const UserImage = ({ profilePic = "", name = "", width = 32, height = 32 }) => {
   const stringAvatar = (name) => {
     return {
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+      children: `${name?.split(" ")?.[0]?.[0]}${name?.split(" ")?.[1]?.[0]}`,
     };
   };
   // Get the access token from cookies and decode it if props are not provided
   const token = Cookies.get("ACCESS_TOKEN");
   const decodedToken = token ? decodeToken(token) : null;
-
-  // Use props if provided, otherwise fall back to the decoded token values
-  const finalProfilePic = profilePic || decodedToken?.profile_pic;
-  const finalName = name || decodedToken?.full_name;
 
   return (
     <>
