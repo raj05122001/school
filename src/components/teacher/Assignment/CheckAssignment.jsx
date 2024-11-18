216a33b06b5f5d9267f2b40689ce84aa2b7db1eb
Django-Blockchained <vishalp7711@gmail.com>
2024-11-18 10:34:48 +0530

    Assessment formula

--- src/components/teacher/Assignment/CheckAssignment.jsx
+++ src/components/teacher/Assignment/CheckAssignment.jsx
@@ -19,7 +19,7 @@ import { useThemeContext } from "@/hooks/ThemeContext";
 import { updateAssignment } from "@/api/apiHelper";
 import { BiSolidRightArrowCircle } from "react-icons/bi";
 import { FaPenNib } from "react-icons/fa";
-
+import MathJax from "react-mathjax2";
 
 const CheckAssignment = ({ assignment, index }) => {
   const { isDarkMode, primaryColor, secondaryColor } = useThemeContext();
@@ -99,118 +99,124 @@ const CheckAssignment = ({ assignment, index }) => {
         boxShadow: 3,
         borderRadius: 2,
         boxShadow: isDarkMode
-                    ? "0px 6px 15px rgba(0, 0, 0, 0.4)"
-                    : "0px 4px 10px #ADD8E6",
+          ? "0px 6px 15px rgba(0, 0, 0, 0.4)"
+          : "0px 4px 10px #ADD8E6",
         backgroundColor: isDarkMode ? "rgba(255, 255, 255, 0.1)" : "white",
         mb: 3,
       }}
     >
-      <Typography mt={4} ml={2} variant="h6" fontWeight={"bold"}>
-        <BiSolidRightArrowCircle
-          style={{ marginRight: "2px", marginTop: "2px" }}
-        />
-        Question {index + 1}:
-      </Typography>
-      <CardContent>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: 1,
-          }}
-        >
-          <Typography variant="h6" gutterBottom color={primaryColor}>
-            {assignment.assignment_que.assignment_text}
+      <MathJax.Context input="tex">
+        <>
+          <Typography mt={4} ml={2} variant="h6" fontWeight={"bold"}>
+            <BiSolidRightArrowCircle
+              style={{ marginRight: "2px", marginTop: "2px" }}
+            />
+            Question {index + 1}:
           </Typography>
-          <Box
-            sx={{
-              width: 30,
-              height: 30,
-              p: 0.8,
-              borderRadius: "50%",
-              backgroundColor: "white",
-              textAlign: "center",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            
-          </Box>
-        </Box>
-        <Typography variant="h6" color="textPrimary">
-             <i>Total Marks:</i> {assignment.assignment_que.assignment_mark}
-        </Typography>
-        <Typography variant="h6" fontWeight={"bold"} mt={4}>
-          <FaPenNib style={{marginRight:"4px"}}/>Submission :
-        </Typography>
-        {assignment?.answer_description && (
-          <Typography variant="body1" color={secondaryColor} sx={{ mt: 2 }}>
-            <strong style={{ color: primaryColor }}>Answer: </strong>
-            {assignment?.answer_description}
-          </Typography>
-        )}
-
-        {renderAnswerContent(assignment)}
-
-        <Box
-          component="form"
-          sx={{
-            mt: 3,
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-          }}
-        >
-          {error && <Alert severity="error">{error}</Alert>}
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={3}>
-              <TextField
-                label="Marks Obtained"
-                type="number"
-                variant="outlined"
-                fullWidth
-                value={grades}
-                onChange={(e) => setGrades(e.target.value)}
-                // disabled={isChecked}
-              />
-            </Grid>
-            <Grid item xs={12} sm={9}>
-              <TextField
-                label="Comment"
-                variant="outlined"
-                multiline
-                fullWidth
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                // disabled={isChecked}
-              />
-            </Grid>
-          </Grid>
-
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleGradeSubmission}
-            sx={{ mt: 2 }}
-            aria-label="Submit Grade"
-            disabled={
-              (isChecked &&
-                comment === assignment?.comment_by_teacher &&
-                Number(grades) === Number(assignment?.marks_obtained)) ||
-              isLoading ||
-              (!comment && !grades)
-            }
-          >
-            {isLoading ? (
-              <CircularProgress size={24} />
-            ) : isChecked ? (
-              "Update"
-            ) : (
-              "Submit"
+          <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: 1,
+              }}
+            >
+              <Typography variant="h6" gutterBottom color={primaryColor}>
+                <MathJax.Text
+                  text={assignment.assignment_que.assignment_text}
+                  inline
+                />
+              </Typography>
+              <Box
+                sx={{
+                  width: 30,
+                  height: 30,
+                  p: 0.8,
+                  borderRadius: "50%",
+                  backgroundColor: "white",
+                  textAlign: "center",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              ></Box>
+            </Box>
+            <Typography variant="h6" color="textPrimary">
+              <i>Total Marks:</i> {assignment.assignment_que.assignment_mark}
+            </Typography>
+            <Typography variant="h6" fontWeight={"bold"} mt={4}>
+              <FaPenNib style={{ marginRight: "4px" }} />
+              Submission :
+            </Typography>
+            {assignment?.answer_description && (
+              <Typography variant="body1" color={secondaryColor} sx={{ mt: 2 }}>
+                <strong style={{ color: primaryColor }}>Answer: </strong>
+                {assignment?.answer_description}
+              </Typography>
             )}
-          </Button>
-        </Box>
-      </CardContent>
+
+            {renderAnswerContent(assignment)}
+
+            <Box
+              component="form"
+              sx={{
+                mt: 3,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              {error && <Alert severity="error">{error}</Alert>}
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    label="Marks Obtained"
+                    type="number"
+                    variant="outlined"
+                    fullWidth
+                    value={grades}
+                    onChange={(e) => setGrades(e.target.value)}
+                    // disabled={isChecked}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                  <TextField
+                    label="Comment"
+                    variant="outlined"
+                    multiline
+                    fullWidth
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    // disabled={isChecked}
+                  />
+                </Grid>
+              </Grid>
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleGradeSubmission}
+                sx={{ mt: 2 }}
+                aria-label="Submit Grade"
+                disabled={
+                  (isChecked &&
+                    comment === assignment?.comment_by_teacher &&
+                    Number(grades) === Number(assignment?.marks_obtained)) ||
+                  isLoading ||
+                  (!comment && !grades)
+                }
+              >
+                {isLoading ? (
+                  <CircularProgress size={24} />
+                ) : isChecked ? (
+                  "Update"
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </Box>
+          </CardContent>
+        </>
+      </MathJax.Context>
     </Card>
   );
 };
