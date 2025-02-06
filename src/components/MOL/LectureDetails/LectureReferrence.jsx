9a8af0ef9732cb260979ba16c9d09e31622fc8a5
Django-Blockchained <vishalp7711@gmail.com>
2025-02-06 12:50:50 +0530

    logo springer

--- src/components/MOL/LectureDetails/LectureReferrence.jsx
+++ src/components/MOL/LectureDetails/LectureReferrence.jsx
@@ -210,7 +210,7 @@ const LectureReferrence = ({ id, isDarkMode }) => {
                       >
                         <Box
                           component="img"
-                          src="/springer_thumbnail.png"
+                          src="/springer_logo.png"
                           alt="Springer logo"
                           sx={{
                             width: 100,
