01164c1e9e4ee4be282cfe03d26929b3ceafae7c
Django-Blockchained <vishalp7711@gmail.com>
2025-05-02 15:03:18 +0530

    MCQ design Changed

--- src/components/MOL/LectureDetails/LectureMCQ.jsx
+++ src/components/MOL/LectureDetails/LectureMCQ.jsx
@@ -192,15 +192,29 @@ const LectureMCQ = ({ id, isDarkMode }) => {
                   variant="contained"
                   onClick={() => setVisibleCount((prevCount) => prevCount + 5)}
                   sx={{
-                    mt: 2,
-                    fontFamily: "Inter",
-                    fontSize: "14px",
-                    backgroundColor: "#E7002A",
-                    textDecoration: "none",
-                    "&:hover": {
-                      backgroundColor: "#E7002A", // customize these shades as needed
-                    },
-                  }}
+                  mt: 2,
+                  display: "inline-flex",
+                  padding: "12px 32px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "8px",
+                  textTransform: "none",
+                  borderRadius: "8px",
+                  background: "#141514",
+                  color: "#FFF",
+                  textAlign: "center",
+                  fontFeatureSettings: "'liga' off, 'clig' off",
+                  fontFamily: "Aptos",
+                  fontSize: "16px",
+                  fontStyle: "normal",
+                  fontWeight: "700",
+                  lineHeight: "24px",
+                  "&:hover": {
+                    border: "1px solid #141514",
+                    background: "#E5E5E5",
+                    color: "#141514",
+                  },
+                }}
                 >
                   Need More
                 </Button>
